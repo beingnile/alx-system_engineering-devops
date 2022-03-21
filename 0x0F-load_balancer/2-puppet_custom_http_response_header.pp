2b44fec40c65aa58fb7bc2e01c844e13fed87038
Nile Okomo <okomonile3@gmail.com>
2022-03-22 01:07:21 +0300

    Revert to commit @3ea4af4

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,15 @@
 # Install nginx using puppet and add custom HTTP header
 exec { 'update':
-  command  => 'apt-get update && apt-get install nginx -y',
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+}
+
+exec { 'chown':
+  command => 'chown -R "$USER":"$USER" /var/www/html',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
@@ -9,9 +18,37 @@ exec { 'change owner':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-exec { 'Add line'
-  command  => 'sudo sed -i "/try_files \$uri \$uri/ =404;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
-  path => '/usr/bin:/usr/sbin:/bin',
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+file { 'config':
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+		add_header X-Served-By \$hostname;
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
 }
 
 exec { 'service nginx restart':
