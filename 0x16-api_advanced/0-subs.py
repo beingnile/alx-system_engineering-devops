22dc8a7d3dca9591515f38144e89221b11d7ac0f
Nile Okomo <okomonile3@gmail.com>
2022-04-26 16:02:48 +0300

    Return the subscribers of a givn subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Queries the Reddit API and returns the
+number of subscribers for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Makes a GET request to the reddit API
+    and returns the number of subscribers for a given subreddit
+
+    Returns:
+        subscribers for a given subreddit
+        0 if the subreddit does not exist
+    """
+
+    headers = {"User-Agent": "Custom"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    subscribers = requests.get(url, headers=headers)
+
+    data = subscribers.json()
+
+    try:
+        return data["data"]["subscribers"]
+    except KeyError:
+        if data["message"] == 'Not Found':
+            return 0
