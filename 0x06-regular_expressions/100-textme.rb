91da43f24209d7f465faf1defa56534ed739cb3a
Nile Okomo <okomonile3@gmail.com>
2023-08-30 12:17:29 +0300

    Clean repo for deferment

--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-regex = /\[from:(?<from>[\+\w\d]+)\] \[to:(?<to>[\+\d]+)\] \[flags:(?<flags>[^\]]+)\]/
-matches = ARGV[0].scan(regex)
-
-values = matches.map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
-puts values.join(",")
