26ec5c56f71d4701f7fd745c83416d80bfac3f7e
Nile Okomo <okomonile3@gmail.com>
2023-11-28 19:28:24 +0300

    Update code to handle repeated commas for missing elements

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\S+)\]|\[to:(\S+)\]|\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:([^\]]*)\]|\[to:([^\]]+)\]|\[flags:([^\]]*)\]/).map { |match| match.compact.reject(&:empty?).first }.join(',')
