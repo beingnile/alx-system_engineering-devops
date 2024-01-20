ceda497adbcf0d935aebaf15acef9c8d320af73f
Nile Okomo <okomonile3@gmail.com>
2024-01-19 18:28:52 -0800

    Add command to install werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Install a package, flask
 exec { 'flask':
-command => 'pip3 install flask==2.1.0',
+command => 'pip3 install werkzeug==2.1.1 && pip3 install flask==2.1.0',
 path    => '/usr/bin',
 }
