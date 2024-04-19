46a0890160f53c71b4873aab4e84d45ab28605e0
Nile Okomo <okomonile3@gmail.com>
2024-04-19 19:10:31 +0300

    Add another package installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Installs flask from pip3
+# Installs Flask and Werkzeug with specific versions
 
 package { 'Flask':
-    ensure   =>  '2.1.0',
-    provider =>  'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
